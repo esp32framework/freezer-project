2fedfc0673c10181fb02b9f8c72b5527a018f8e6
DiegoCivi <dcivini@fi.uba.ar>
2024-11-06 20:27:59 -0300

    Deleted test api endpoints
    Co-authored-by: Joaquin <jorivero@fi.uba.ar>

--- package/src/app/api/test2/route.ts
+++
@@ -1,8 +0,0 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
- 
-export async function GET(request: Request) {
-  //const pets = await sql`SELECT time_tst AT TIME ZONE 'UTC' AS time_tst FROM time_test WHERE time_tst ;`;
-  const measurements = await sql`SELECT * FROM espdata ORDER BY time DESC LIMIT 15;`;
-  return NextResponse.json({ measurements }, { status: 200 });
-}
