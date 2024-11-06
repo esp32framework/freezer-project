a061db4306428b1bb95375cd0398f8711664b69e
DiegoCivi <dcivini@fi.uba.ar>
2024-11-06 18:57:52 -0300

    Force dynamic on api/door/get

--- package/src/app/api/door/get/route.ts
+++ package/src/app/api/door/get/route.ts
@@ -1,6 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
     const doors_data = await sql`
     SELECT *
