65886f9580b54ba85977c54c36ecbfce51e365a7
DiegoCivi <dcivini@fi.uba.ar>
2024-11-06 19:29:27 -0300

    Fix get query for doors api
    Co-authored-by: Joaquin <jorivero@fi.uba.ar>

--- package/src/app/api/door/get/route.ts
+++ package/src/app/api/door/get/route.ts
@@ -1,17 +1,16 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 export async function GET(request: Request) {
-    const doors_data = await sql`
+  const doors_data = await sql`
     SELECT *
-    FROM doors
-    WHERE time in (
-	    SELECT MAX(time)
-        FROM doors
-        GROUP BY espid
-    )`;
-    
-    return NextResponse.json({ doors_data }, { status: 200 });
+    FROM doors AS d1
+    WHERE time = (
+        SELECT MAX(time)
+        FROM doors AS d2
+        WHERE d1.espid = d2.espid
+    );`;
+
+  return NextResponse.json({ doors_data }, { status: 200 });
 }
-  
